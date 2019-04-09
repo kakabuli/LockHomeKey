cf532f3b50dc3c04c8b8504030746dd54cea3ec7
fujihui <fujihui@kaadas.com>
2019-04-09 17:02:28 +0800

    修改

--- app/src/main/java/com/kaadas/lock/activity/addDevice/cateye/AddDeviceCatEyeSecondActivity.java
+++ app/src/main/java/com/kaadas/lock/activity/addDevice/cateye/AddDeviceCatEyeSecondActivity.java
@@ -11,6 +11,6 @@ public class AddDeviceCatEyeSecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.);
+
     }
 }
