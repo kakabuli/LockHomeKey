56e340b9494ce12cf0e97067091ddf4953a13ad7
1164864735@qq.com <04155698zsw>
2019-04-11 08:45:06 +0800

    蓝牙锁功能页面和逻辑,蓝牙锁授权页面和逻辑,设备信息页面和逻辑

---
+++ app/src/main/java/com/kaadas/lock/bean/BluetoothLockFunctionBean.java
@@ -0,0 +1,40 @@
+package com.kaadas.lock.bean;
+
+public class BluetoothLockFunctionBean {
+
+    private int image;
+
+    private String name;
+
+    private String number;
+
+    public BluetoothLockFunctionBean(int image, String name, String number) {
+        this.image = image;
+        this.name = name;
+        this.number = number;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
