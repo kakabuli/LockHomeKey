22ef2d26e75e62ba13df2d2ed1f5a53b1cbc80d8
1164864735@qq.com <04155698zsw>
2019-04-24 11:18:49 +0800

    共享设备模块页面和逻辑

--- app/src/main/java/com/kaadas/lock/activity/device/gateway/GatewayLockFunctionActivity.java
+++ app/src/main/java/com/kaadas/lock/activity/device/gateway/GatewayLockFunctionActivity.java
@@ -14,6 +14,7 @@ import com.kaadas.lock.R;
 import com.kaadas.lock.activity.device.gateway.card.GatewayDoorCardManagerActivity;
 import com.kaadas.lock.activity.device.gateway.fingerprint.GatewayFingerprintManagerActivity;
 import com.kaadas.lock.activity.device.gateway.password.GatewayPasswordManagerActivity;
+import com.kaadas.lock.activity.device.gateway.share.GatewaySharedDeviceManagementActivity;
 import com.kaadas.lock.bean.BluetoothLockFunctionBean;
 import com.kaadas.lock.utils.DateUtils;
 import com.kaadas.lock.utils.KeyConstants;
@@ -208,8 +209,8 @@ public class GatewayLockFunctionActivity extends AppCompatActivity implements Vi
                 startActivity(intent);
                 break;
             case R.id.ll_four:
-//                intent = new Intent(this, BluetoothSharedDeviceManagementActivity.class);
-//                startActivity(intent);
+                intent = new Intent(this, GatewaySharedDeviceManagementActivity.class);
+                startActivity(intent);
                 break;
             case R.id.ll_five:
 //                intent = new Intent(this, BluetoothMoreActivity.class);
