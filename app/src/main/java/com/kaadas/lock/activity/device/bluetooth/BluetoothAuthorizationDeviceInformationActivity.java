2a6dc56a6407d220ef3f7b8600fd966187065710
1164864735@qq.com <04155698zsw>
2019-05-13 11:06:24 +0800

    锁授权信息页面更改

--- app/src/main/java/com/kaadas/lock/activity/device/bluetooth/BluetoothAuthorizationDeviceInformationActivity.java
+++ app/src/main/java/com/kaadas/lock/activity/device/bluetooth/BluetoothAuthorizationDeviceInformationActivity.java
@@ -39,8 +39,8 @@ public class BluetoothAuthorizationDeviceInformationActivity extends BaseBleActi
     RelativeLayout rlBluetoothModuleVersion;
     @BindView(R.id.tv_bluetooth_module_version)
     TextView tvBluetoothModuleVersion;
-    @BindView(R.id.tv_software_version)
-    TextView tvSoftwareVersion;
+    @BindView(R.id.tv_lock_software_version)
+    TextView tvLockSoftwareVersion;
     private BleLockInfo bleLockInfo;
 
     @Override
@@ -78,7 +78,11 @@ public class BluetoothAuthorizationDeviceInformationActivity extends BaseBleActi
 
     @Override
     public void SoftwareRevDataSuccess(String data) {
-        tvBluetoothModuleVersion.setText(data);
+        String[] split = data.split("_");
+        String strModuleHardwareVersion = split[0];
+        String strLockHardwareVersion = split[1];
+        tvLockSoftwareVersion.setText(strLockHardwareVersion);
+        tvBluetoothModuleVersion.setText(strModuleHardwareVersion);
     }
 
     @Override
@@ -91,7 +95,7 @@ public class BluetoothAuthorizationDeviceInformationActivity extends BaseBleActi
         String[] split = data.split("_");
         String strModuleHardwareVersion = split[0];
         String strLockHardwareVersion = split[1];
-        tvBluetoothModuleVersion.setText(strModuleHardwareVersion);
+//        tvBluetoothModuleVersion.setText(strModuleHardwareVersion);
         tvLockFirmwareVersion.setText(strLockHardwareVersion);
     }
 
@@ -122,7 +126,6 @@ public class BluetoothAuthorizationDeviceInformationActivity extends BaseBleActi
 
     @Override
     public void ModelNumberDataSuccess(String data) {
-        tvSoftwareVersion.setText(data);
         hiddenLoading();
     }
 
