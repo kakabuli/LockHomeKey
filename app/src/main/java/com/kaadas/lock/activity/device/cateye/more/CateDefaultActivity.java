5dd1ee3348c9912f133ebe8c33982c63162180bc
fujihui <fujihui@kaadas.com>
2019-05-15 15:57:24 +0800

    修改

--- app/src/main/java/com/kaadas/lock/activity/device/cateye/more/CateDefaultActivity.java
+++ app/src/main/java/com/kaadas/lock/activity/device/cateye/more/CateDefaultActivity.java
@@ -1,6 +1,5 @@
 package com.kaadas.lock.activity.device.cateye.more;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -14,7 +13,6 @@ import com.kaadas.lock.MyApplication;
 import com.kaadas.lock.R;
 import com.kaadas.lock.mvp.mvpbase.BaseActivity;
 import com.kaadas.lock.mvp.presenter.cateye.CatEyeDefaultPresenter;
-import com.kaadas.lock.mvp.view.IFingerprintManagerView;
 import com.kaadas.lock.mvp.view.cateye.ICatEyeDefaultView;
 import com.kaadas.lock.publiclibrary.mqtt.publishbean.GetPirSlientBean;
 import com.kaadas.lock.utils.KeyConstants;
@@ -43,12 +41,15 @@ public class CateDefaultActivity extends BaseActivity<ICatEyeDefaultView, CatEye
     RelativeLayout rlDeviceName;
     @BindView(R.id.save)
     Button save;
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
     private String gatewayId;
     private String deviceId;
     private LoadingDialog loadingDialog;
 
     private int enable = -1;
-    private boolean getPirEnableFlag=false;
+    private boolean getPirEnableFlag = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,7 +69,7 @@ public class CateDefaultActivity extends BaseActivity<ICatEyeDefaultView, CatEye
     private void initListener() {
         iv_default_monitor.setOnClickListener(this);
         save.setOnClickListener(this);
-
+        ivBack.setOnClickListener(this);
     }
 
     private void initData() {
@@ -89,6 +90,9 @@ public class CateDefaultActivity extends BaseActivity<ICatEyeDefaultView, CatEye
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.iv_back:
+                finish();
+                break;
             case R.id.iv_default_monitor:
                 //关闭Enable
                 if (enable == 1) {
@@ -100,49 +104,49 @@ public class CateDefaultActivity extends BaseActivity<ICatEyeDefaultView, CatEye
                 }
                 break;
             case R.id.save:
-                if (getPirEnableFlag==false){
+                if (getPirEnableFlag == false) {
                     ToastUtil.getInstance().showShort(getString(R.string.get_pir_slient_fail));
                     return;
                 }
 
-                String periodtimeStr=periodtime.getText().toString().trim();
-                String thresholdStr=threshold.getText().toString().trim();
-                String protecttimeStr=protecttime.getText().toString().trim();
-                String ustStr=ust.getText().toString().trim();
-                String maxprohibitionStr=maxprohibition.getText().toString().trim();
-                if (TextUtils.isEmpty(periodtimeStr)||TextUtils.isEmpty(thresholdStr)||TextUtils.isEmpty(protecttimeStr)||TextUtils.isEmpty(ustStr)||TextUtils.isEmpty(maxprohibitionStr)){
+                String periodtimeStr = periodtime.getText().toString().trim();
+                String thresholdStr = threshold.getText().toString().trim();
+                String protecttimeStr = protecttime.getText().toString().trim();
+                String ustStr = ust.getText().toString().trim();
+                String maxprohibitionStr = maxprohibition.getText().toString().trim();
+                if (TextUtils.isEmpty(periodtimeStr) || TextUtils.isEmpty(thresholdStr) || TextUtils.isEmpty(protecttimeStr) || TextUtils.isEmpty(ustStr) || TextUtils.isEmpty(maxprohibitionStr)) {
                     ToastUtil.getInstance().showShort(getString(R.string.parameter_not_empty));
                     return;
                 }
-                int periodtimeInt=Integer.parseInt(periodtimeStr);
-                if (periodtimeInt>60||periodtimeInt<=0){
+                int periodtimeInt = Integer.parseInt(periodtimeStr);
+                if (periodtimeInt > 60 || periodtimeInt <= 0) {
                     ToastUtil.getInstance().showShort(getString(R.string.please_input_legal_number));
                     return;
                 }
 
-                int thresholdInt=Integer.parseInt(thresholdStr);
-                if (thresholdInt>60||thresholdInt<=0){
+                int thresholdInt = Integer.parseInt(thresholdStr);
+                if (thresholdInt > 60 || thresholdInt <= 0) {
                     ToastUtil.getInstance().showShort(getString(R.string.please_input_legal_number));
                     return;
                 }
-                int protecttimeInt=Integer.parseInt(protecttimeStr);
-                if (protecttimeInt>60||periodtimeInt<=0){
+                int protecttimeInt = Integer.parseInt(protecttimeStr);
+                if (protecttimeInt > 60 || periodtimeInt <= 0) {
                     ToastUtil.getInstance().showShort(getString(R.string.please_input_legal_number));
                     return;
                 }
-                int ustInt=Integer.parseInt(ustStr);
-                if (ustInt>60||ustInt<=0){
+                int ustInt = Integer.parseInt(ustStr);
+                if (ustInt > 60 || ustInt <= 0) {
                     ToastUtil.getInstance().showShort(getString(R.string.please_input_legal_number));
                     return;
                 }
 
-                int maxprohibitionInt=Integer.parseInt(maxprohibitionStr);
-                if (maxprohibitionInt>60||maxprohibitionInt<=0){
+                int maxprohibitionInt = Integer.parseInt(maxprohibitionStr);
+                if (maxprohibitionInt > 60 || maxprohibitionInt <= 0) {
                     ToastUtil.getInstance().showShort(getString(R.string.please_input_legal_number));
                     return;
                 }
                 loadingDialog.show(getString(R.string.take_effect_be_being));
-                mPresenter.setPirSlient(MyApplication.getInstance().getUid(),gatewayId,deviceId,ustInt,enable,maxprohibitionInt,periodtimeInt,protecttimeInt,thresholdInt);
+                mPresenter.setPirSlient(MyApplication.getInstance().getUid(), gatewayId, deviceId, ustInt, enable, maxprohibitionInt, periodtimeInt, protecttimeInt, thresholdInt);
                 break;
         }
 
@@ -150,7 +154,7 @@ public class CateDefaultActivity extends BaseActivity<ICatEyeDefaultView, CatEye
 
     @Override
     public void getPirSlientSuccess(GetPirSlientBean.ReturnDataBean dataBean) {
-        getPirEnableFlag=true;
+        getPirEnableFlag = true;
         if (loadingDialog != null) {
             loadingDialog.dismiss();
         }
@@ -172,10 +176,10 @@ public class CateDefaultActivity extends BaseActivity<ICatEyeDefaultView, CatEye
         if (iv_default_monitor != null) {
             if (dataBean.getEnable() == 1) {
                 iv_default_monitor.setImageResource(R.mipmap.iv_open);
-                enable=1;
+                enable = 1;
             } else {
                 iv_default_monitor.setImageResource(R.mipmap.iv_close);
-                enable=0;
+                enable = 0;
             }
         }
 
@@ -183,7 +187,7 @@ public class CateDefaultActivity extends BaseActivity<ICatEyeDefaultView, CatEye
 
     @Override
     public void getPirSlientFail() {
-        getPirEnableFlag=false;
+        getPirEnableFlag = false;
         if (loadingDialog != null) {
             loadingDialog.dismiss();
         }
@@ -193,7 +197,7 @@ public class CateDefaultActivity extends BaseActivity<ICatEyeDefaultView, CatEye
 
     @Override
     public void getPirSlientThrowable(Throwable throwable) {
-        getPirEnableFlag=false;
+        getPirEnableFlag = false;
         if (loadingDialog != null) {
             loadingDialog.dismiss();
         }
@@ -203,14 +207,14 @@ public class CateDefaultActivity extends BaseActivity<ICatEyeDefaultView, CatEye
 
     @Override
     public void setPirSlientSuccess() {
-        if (loadingDialog!=null){
+        if (loadingDialog != null) {
             loadingDialog.dismiss();
         }
     }
 
     @Override
     public void setPirSlientFail() {
-        if (loadingDialog!=null){
+        if (loadingDialog != null) {
             loadingDialog.dismiss();
         }
         ToastUtil.getInstance().showShort(getString(R.string.set_failed));
@@ -218,7 +222,7 @@ public class CateDefaultActivity extends BaseActivity<ICatEyeDefaultView, CatEye
 
     @Override
     public void setPirSlientThrowable(Throwable throwable) {
-        if (loadingDialog!=null){
+        if (loadingDialog != null) {
             loadingDialog.dismiss();
         }
         ToastUtil.getInstance().showShort(getString(R.string.set_failed));
