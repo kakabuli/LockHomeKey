df92f9bcef2ad8befa0889a443ebd775a28bfe55
fujihui <fujihui@kaadas.com>
2019-05-31 16:30:28 +0800

    增加v6,v7图片

--- app/src/main/java/com/kaadas/lock/adapter/DeviceDetailAdapter.java
+++ app/src/main/java/com/kaadas/lock/adapter/DeviceDetailAdapter.java
@@ -145,7 +145,11 @@ public class DeviceDetailAdapter extends BaseQuickAdapter<HomeShowBean, BaseView
                         helper.setImageResource(R.id.device_image, R.mipmap.qz013);
                     }else if (model.contains("S8")){
                         helper.setImageResource(R.id.device_image, R.mipmap.s8);
-                    }else{
+                    }else if (model.contains("V6")) {
+                        helper.setImageResource(R.id.device_image, R.mipmap.v6);
+                    }else if (model.contains("V7")) {
+                        helper.setImageResource(R.id.device_image, R.mipmap.v7);
+                    } else{
                         helper.setImageResource(R.id.device_image, R.mipmap.default_zigbee_lock_icon);
                     }
                     batteryView.setPower(blePower);
