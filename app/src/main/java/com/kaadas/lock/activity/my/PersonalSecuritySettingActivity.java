de3f0c57563257ee4fb849c99684edf3c3b559e1
fujihui <fujihui@kaadas.com>
2019-06-05 10:45:59 +0800

    修改touchID

--- app/src/main/java/com/kaadas/lock/activity/my/PersonalSecuritySettingActivity.java
+++ app/src/main/java/com/kaadas/lock/activity/my/PersonalSecuritySettingActivity.java
@@ -1,39 +1,24 @@
 package com.kaadas.lock.activity.my;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.hardware.fingerprint.FingerprintManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.CancellationSignal;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.kaadas.lock.MyApplication;
 import com.kaadas.lock.R;
-import com.kaadas.lock.activity.MainActivity;
-import com.kaadas.lock.activity.login.PersonalVerifyFingerPrintActivity;
-import com.kaadas.lock.activity.login.PersonalVerifyGesturePasswordActivity;
-import com.kaadas.lock.activity.login.SplashActivity;
 import com.kaadas.lock.mvp.mvpbase.BaseActivity;
 import com.kaadas.lock.mvp.presenter.personalpresenter.PersonalSecuritySettingPresenter;
+import com.kaadas.lock.mvp.view.personalview.IPersonalSecuritySettingView;
 import com.kaadas.lock.utils.AlertDialogUtil;
-import com.kaadas.lock.utils.EncryUtils;
 import com.kaadas.lock.utils.KeyConstants;
 import com.kaadas.lock.utils.ToastUtil;
 import com.kaadas.lock.utils.cachefloder.ACache;
 import com.kaadas.lock.utils.cachefloder.CacheFloder;
-import com.kaadas.lock.mvp.view.personalview.IPersonalSecuritySettingView;
-import com.kaidishi.lock.WelcomeActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -62,11 +47,9 @@ public class PersonalSecuritySettingActivity extends BaseActivity<IPersonalSecur
     RelativeLayout rlOpenHandPwd;
     @BindView(R.id.rl_open_touch_id)
     RelativeLayout rlOpenTouchId;
-    private FingerprintManager mFingerprintManager;
-    private CancellationSignal mCancellationSignal;//用于取消指纹识别
-    private final String mAlias = "touch_id_key";//用于获取加密key
-    AlertDialog alertDialog;
-    AlertDialog verificationFailDialog;
+    @BindView(R.id.touch_id_status)
+    TextView touchIdStatus;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,98 +57,24 @@ public class PersonalSecuritySettingActivity extends BaseActivity<IPersonalSecur
         ButterKnife.bind(this);
         initView();
         initListener();
-        ivBack.setOnClickListener(this);
-        tvContent.setText(R.string.security_setting);
     }
-    private void initTouchId() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            mFingerprintManager = getFingerprintManagerOrNull();
-            if (mFingerprintManager != null) {
-                mCancellationSignal = new CancellationSignal();
-                //开始验证指纹
-                mFingerprintManager.authenticate(new
-                                FingerprintManager.CryptoObject(EncryUtils.getInstance().getCipher(mAlias))
-                        , mCancellationSignal, 0, callback, null);
-            }
-        }
-    }
-    /**
-     * 指纹识别回调监听
-     */
-    @TargetApi(23)
-    private FingerprintManager.AuthenticationCallback callback = new FingerprintManager.AuthenticationCallback() {
-        @Override
-        public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
-        if (touchId){
-            ivOpenTouchId.setImageResource(R.mipmap.iv_open);
-            if (alertDialog!=null){
-                alertDialog.dismiss();
-            }
-        }else {
-            ivOpenTouchId.setImageResource(R.mipmap.iv_close);
-            ACache.get(MyApplication.getInstance()).remove(MyApplication.getInstance().getUid() + "fingerStatus");
-            if (alertDialog!=null){
-                alertDialog.dismiss();
-            }
-        }
-        if (verificationFailDialog!=null){
-            verificationFailDialog.dismiss();
-        }
 
 
-        }
-
-        @Override
-        public void onAuthenticationError(int errorCode, CharSequence errString) {
-//            ToastUtil.getInstance().showShort(R.string.fingerprint_fail);
-        }
-
-        @Override
-        public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
-            //指纹验证失败，可再验，可能手指过脏，或者移动过快等原因。
-            ToastUtil.getInstance().showShort(R.string.fingerprint_fail_check);
-        }
-
-        @Override
-        public void onAuthenticationFailed() {
-            //指纹验证失败，指纹识别失败，可再验，该指纹不是系统录入的指纹。
-            if (verificationFailDialog!=null&&verificationFailDialog.isShowing()){
-
-            }else {
-                verificationFail();
-            }
-            ToastUtil.getInstance().showShort(R.string.fingerprint_unidentifiable);
-        }
-    };
-    /**
-     * 获取FingerprintManager
-     *
-     * @return FingerprintManager
-     */
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public FingerprintManager getFingerprintManagerOrNull() {
-        if (getApplication().getPackageManager().hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)) {
-            return getApplication().getSystemService(FingerprintManager.class);
-        } else {
-            return null;
-        }
-    }
     @Override
     protected PersonalSecuritySettingPresenter<IPersonalSecuritySettingView> createPresent() {
-
         return new PersonalSecuritySettingPresenter<>();
     }
 
     private void initListener() {
         rlOpenHandPwd.setOnClickListener(this);
         rlOpenTouchId.setOnClickListener(this);
-
+        ivBack.setOnClickListener(this);
     }
 
     private void initView() {
-
         mPresenter.setHandPwdSwitchFlag();
         mPresenter.setFingerPrintFlag();
+        tvContent.setText(R.string.security_setting);
     }
 
     @OnClick({R.id.update_hand_pwd_layout})
@@ -227,47 +136,18 @@ public class PersonalSecuritySettingActivity extends BaseActivity<IPersonalSecur
     @Override
     public void phoneFigerprintOpen() {
         //已经打开
-//        touchId = true;
-//        ivOpenTouchId.setImageResource(R.mipmap.iv_open);
-        View mView = LayoutInflater.from(this).inflate(R.layout.activity_personal_fingerprint_security, null);
-        TextView mFingerCancel = mView.findViewById(R.id.finger_cancel);
-            alertDialog = AlertDialogUtil.getInstance().common(this, mView);
-            alertDialog.setCanceledOnTouchOutside(false);
+        touchId = true;
+        ivOpenTouchId.setImageResource(R.mipmap.iv_open);
+        touchIdStatus.setText(getString(R.string.close_touch_id));
         CacheFloder.writePhoneFingerPrint(ACache.get(MyApplication.getInstance()), MyApplication.getInstance().getUid() + "fingerStatus", "true");
-        mFingerCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                alertDialog.dismiss();
-            }
-        });
     }
-
-    private void verificationFail() {
-        View mView = LayoutInflater.from(this).inflate(R.layout.activity_personal_fingerprint_security_fail, null);
-        TextView tvLeft = mView.findViewById(R.id.tv_left);
-        TextView tvRight = mView.findViewById(R.id.tv_right);
-            verificationFailDialog = AlertDialogUtil.getInstance().common(this, mView);
-        verificationFailDialog.setCanceledOnTouchOutside(false);
-        tvLeft.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                verificationFailDialog.dismiss();
-            }
-        });
-        tvRight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                verificationFailDialog.dismiss();
-            }
-        });
-    }
-
 
     @Override
     public void phoneFigerprintClose() {
         //关闭
-//        touchId = false;
-//        ivOpenTouchId.setImageResource(R.mipmap.iv_close);
+        touchId = false;
+        ivOpenTouchId.setImageResource(R.mipmap.iv_close);
+        touchIdStatus.setText(getString(R.string.open_touch_id));
         ToastUtil.getInstance().showLong(R.string.no_open_fingerprint);
     }
 
@@ -275,12 +155,14 @@ public class PersonalSecuritySettingActivity extends BaseActivity<IPersonalSecur
     public void openFingerPrintSuccess() {
         touchId = true;
         ivOpenTouchId.setImageResource(R.mipmap.iv_open);
+        touchIdStatus.setText(getString(R.string.close_touch_id));
     }
 
     @Override
     public void closeFingerPrintSuccess() {
         touchId = false;
         ivOpenTouchId.setImageResource(R.mipmap.iv_close);
+        touchIdStatus.setText(getString(R.string.open_touch_id));
     }
 
     @Override
@@ -298,10 +180,8 @@ public class PersonalSecuritySettingActivity extends BaseActivity<IPersonalSecur
                     startActivity(open);
                 } else {
                     Intent personalUpdateVerifyIntent = new Intent(this, PersonalUpdateVerifyGesturePwd.class);
-                    personalUpdateVerifyIntent.putExtra(KeyConstants.SOURCE,"PersonalSecuritySettingActivity");
-                    startActivityForResult(personalUpdateVerifyIntent,100);
-
-
+                    personalUpdateVerifyIntent.putExtra(KeyConstants.SOURCE, "PersonalSecuritySettingActivity");
+                    startActivityForResult(personalUpdateVerifyIntent, 100);
                 }
                 break;
             case R.id.rl_open_touch_id:
@@ -313,17 +193,15 @@ public class PersonalSecuritySettingActivity extends BaseActivity<IPersonalSecur
                     if (flag == false) {
                         //手机不支持指纹识别
                         ivOpenTouchId.setImageResource(R.mipmap.iv_close);
+                        touchIdStatus.setText(getString(R.string.open_touch_id));
                         ToastUtil.getInstance().showShort(R.string.no_support_fingeprint);
                     } else {
                         mPresenter.isOpenFingerPrint();
-                        initTouchId();
                     }
                 } else {
-                    mPresenter.isOpenFingerPrint();
-                    initTouchId();
-//                    ivOpenTouchId.setImageResource(R.mipmap.iv_close);
-//                    ACache.get(MyApplication.getInstance()).remove(MyApplication.getInstance().getUid() + "fingerStatus");
-
+                    ivOpenTouchId.setImageResource(R.mipmap.iv_close);
+                    touchIdStatus.setText(getString(R.string.open_touch_id));
+                    ACache.get(MyApplication.getInstance()).remove(MyApplication.getInstance().getUid() + "fingerStatus");
                 }
                 break;
         }
@@ -333,16 +211,13 @@ public class PersonalSecuritySettingActivity extends BaseActivity<IPersonalSecur
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == Activity.RESULT_OK) {
-            if (100==requestCode){
-//关闭
+            if (100 == requestCode) {
                 //清除缓存手势密码数据
                 ivOpenHandPwd.setImageResource(R.mipmap.iv_close);
                 ACache.get(MyApplication.getInstance()).remove(MyApplication.getInstance().getUid() + "handPassword");
                 securitySettingSwitchText.setText(R.string.open_hand_pwd);
-            }else if (200==requestCode){
-                ivOpenTouchId.setImageResource(R.mipmap.iv_close);
-                ACache.get(MyApplication.getInstance()).remove(MyApplication.getInstance().getUid() + "fingerStatus");
             }
         }
     }
+
 }
