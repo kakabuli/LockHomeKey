a17cfc2af41b329f9cc0dbcafb8005e490dedd74
1164864735@qq.com <04155698zsw>
2019-06-05 13:45:41 +0800

    处理手势密码提示错误的问题

--- app/src/main/java/com/kaadas/lock/activity/login/PersonalVerifyGesturePasswordActivity.java
+++ app/src/main/java/com/kaadas/lock/activity/login/PersonalVerifyGesturePasswordActivity.java
@@ -14,6 +14,7 @@ import com.kaadas.lock.activity.MainActivity;
 import com.kaadas.lock.MyApplication;
 import com.kaadas.lock.R;
 import com.kaadas.lock.mvp.mvpbase.BaseAddToApplicationActivity;
+import com.kaadas.lock.utils.KeyConstants;
 import com.kaadas.lock.utils.ToastUtil;
 import com.kaadas.lock.utils.cachefloder.ACache;
 import com.kaadas.lock.utils.cachefloder.CacheFloder;
@@ -43,6 +44,11 @@ public class PersonalVerifyGesturePasswordActivity extends BaseAddToApplicationA
         setContentView(R.layout.personal_verify_hand_pwd);
         ButterKnife.bind(this);
         initView();
+        Intent intent = getIntent();
+        String source = intent.getStringExtra(KeyConstants.SOURCE);
+        if ("WelcomeActivity".equals(source)){
+            mTextTip.setText(R.string.draw_gesture_password);
+        }
     }
 
     private void initView() {
