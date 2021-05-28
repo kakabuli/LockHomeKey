0078587c223b841263d68d78f074f7c517369f4c
liuhaifeng <liuhaifeng@kaadas.com>
2021-05-28 09:01:29 +0800

    新增视频锁型号搜索

--- app/src/main/java/com/kaadas/lock/utils/WiFiLockUtils.java
+++ app/src/main/java/com/kaadas/lock/utils/WiFiLockUtils.java
@@ -45,7 +45,8 @@ public class WiFiLockUtils {
         ,"S110-D","S110D","S110 D","S110_D","X9","X9-F","X90","X9F"});
         // -----------------------------------------
         PAIRING_MODE.put("WiFi&VIDEO",new String[]{"K10V","K20V","K20-V","X9-V","X9V","兰博基尼传奇可视猫眼智能锁","K13V","K10FV"
-                ,"K20 Max 3D人脸可视猫眼智能锁"});
+                ,"K20 Max 3D人脸可视猫眼智能锁","X9 Pro 猫眼视屏智能锁","X9 Pro Max 3D人脸猫眼视屏智能锁","K60 FVP 3D人脸猫眼视屏智能锁"
+                ,"K60 VP 猫眼视屏智能锁","K3A1A","K3A0A","K60 FVP","K60 VP"});
     }
     /**
      * 根据型号判断对应的配网方式
